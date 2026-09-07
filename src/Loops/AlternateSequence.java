042055611c30a8b05156298537ffc45c5664873e
Aniket Singh <thakuraniketsingh80@gmail.com>
2026-09-08 00:35:48 +0530

    Changing code of Alternate Series

--- src/Loops/AlternateSequence.java
+++ src/Loops/AlternateSequence.java
@@ -6,20 +6,15 @@ public class AlternateSequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num , n , count = 1;
+        int num, n, count = 1;
 
         System.out.print("Enter your number: ");
         num = sc.nextInt();
         n = num;
-        for (int i = 1; i <=num ; i++) {
-            if (i % 2  == 0) {
-                System.out.println(n);
-                n--;
-            }
-            else {
-                System.out.println(count);
-                count++;
-            }
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + 1);
+            System.out.println(n - i);
         }
     }
 }
+
