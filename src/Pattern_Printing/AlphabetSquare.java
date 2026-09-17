ffc99e3bbb016a369569e790a3033e4fb1fff78e
Aniket Singh <thakuraniketsingh80@gmail.com>
2026-09-17 19:45:27 +0530

    Adding  patterns

---
+++ src/Pattern_Printing/AlphabetSquare.java
@@ -0,0 +1,19 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+        System.out.print("Enter your rows: ");
+        row =  sc.nextInt();
+        for (int i = 1; i <= row ; i++) {
+            for (int j = 1; j <= row; j++) {
+            System.out.print((char)(64 + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
