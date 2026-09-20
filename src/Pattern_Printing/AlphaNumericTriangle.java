32cee33310f83e6e4f4c6300bb95b70ffdf5710b
Aniket Singh <thakuraniketsingh80@gmail.com>
2026-09-20 16:02:07 +0530

    Adding stage-3 patterns

---
+++ src/Pattern_Printing/AlphaNumericTriangle.java
@@ -0,0 +1,26 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphaNumericTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+
+        System.out.print("Enter your row: ");
+        row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % 2 == 0){
+                    System.out.print((char)(64 + j));
+                }
+                else {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern_Printing/AlphabetTriangle.java
@@ -0,0 +1,20 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class AlphabetTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+
+        System.out.print("Enter your rows: ");
+        row = sc.nextInt();
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(64 + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
