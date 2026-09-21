9db2bc0e786718faab7a9caea5561e66f8130329
Aniket Singh <thakuraniketsingh80@gmail.com>
2026-09-21 20:48:29 +0530

    Adding pattern

---
+++ src/Pattern_Printing/FlippedNumberTriangle.java
@@ -0,0 +1,21 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+
+public class FlippedNumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+
+        System.out.print("Enter your row: ");
+        row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row + 1 - i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
